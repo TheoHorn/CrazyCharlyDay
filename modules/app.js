b24ab70cfb1fdfe7531525864a00772a7149e274
TheoHorn <82098718+TheoHorn@users.noreply.github.com>
2022-03-10 15:39:23 +0100

    panier correction

--- modules/app.js
+++ modules/app.js
@@ -1,7 +1,7 @@
 import * as ui_cart from "./ui_cart.js";
 
 export let init = function(){
-    let cart = document.getElementById("dropdown");
+    let cart = document.getElementsByClassName("dropdown")[0];
     cart.addEventListener("click", (e) => {
         ui_cart.cart_toggle_visibility()
         console.log(e.target)
