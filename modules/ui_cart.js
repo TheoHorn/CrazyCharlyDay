3e9e74e52eec8181c8b6a5aabb52e01629e4dca0
Louis <82098276+MonsieurMiam@users.noreply.github.com>
2022-03-10 16:29:54 +0100

    Update ui_cart.js

--- modules/ui_cart.js
+++ modules/ui_cart.js
@@ -34,10 +34,32 @@ export const cart_toggle_visibility = () => {
 
 }
 
+const displayCart = function () {
+    document.getElementById('qty').innerHTML=genericCalc((x,y)=>x+y.qty)
+    let affCart = cart.tabCart.map(ob =>{
+        return `<div class="product-img">
+				    <img src="./img/produits/1.jpg" alt="">
+				</div>
+				<div class="product-body">
+					<h3 class="product-name"><a href="#">Nom produit</a></h3>
+					<h4 class="product-poids"><span class="qty">${ob.qty}</span>Prix</h4>
+				</div>
+				<button class="delete"><i class="fa fa-close"></i></button>`;
+    });
+    const reducer = (previousValue, currenValue) => previousValue + currenValue;
+    if(affCart.length!==0) {
+        document.getElementById('product-widget').innerHTML = affCart.reduce(reducer)
+    }else{
+        document.getElementById('product-widget').innerHTML = ``;
+    }
+
+    document.getElementById('cart-total').innerHTML=
+        `<small>${genericCalc((x,y)=>x+y.qty)}</small>
+            <h5>Total: prix</h5>`
+}
+
 export default {
-    tabCart : tabCart,
     addToCart,
-    genericCalc,
-    emptyCart
+    displayCart
 }
 
